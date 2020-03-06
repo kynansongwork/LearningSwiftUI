13df0feadc9390fb0be05dd393c5f9baefab66be
Kynan Song <kynan.song@xdesign.com>
2020-03-06 15:24:21 +0000

    Starting Ray Wenderlich tutorial

--- LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/ContentView.swift
@@ -9,13 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                VStack{
+                    Rectangle().foregroundColor(Color(red: rTarget, green: gTarget, blue: bTarget, opacity: 1.0))
+                    Text("Match this colour")
+                }
+                
+                VStack {
+                    Rectangle().foregroundColor(Color(red: rGuess, green: gGuess, blue: bGuess, opacity: 1.0))
+                    HStack {
+                        Text("Red: xxx")
+                        Text("Green: xxx")
+                        Text("Blue: xxx")
+                    }
+                }
+            }
+            
+            Text("Hit me button")
+            
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
     }
 }
