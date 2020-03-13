f48c9eb3676f234711851a0596e34819bab1b72a
Kynan Song <kynan.song@xdesign.com>
2020-03-13 15:54:17 +0000

    added coordinator

---
+++ LearningSwiftUI/User Interface/SwiftUI View/ColourUISlider.swift
@@ -0,0 +1,49 @@
+//
+//  ColourUISlider.swift
+//  LearningSwiftUI
+//
+//  Created by Kynan Song on 13/03/2020.
+//  Copyright © 2020 xDesign. All rights reserved.
+//
+
+import SwiftUI
+
+struct ColourUISlider: UIViewRepresentable {
+    var colour: UIColor
+    @Binding var value: Double
+    
+    func makeCoordinator() -> ColourUISlider.Coordinator {
+        Coordinator(value: $value)
+    }
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider(frame: .zero)
+        slider.thumbTintColor = colour
+        slider.value = Float(value)
+        slider.minimumTrackTintColor = .green
+        slider.maximumTrackTintColor = .blue
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = Float(self.value)
+    }
+    
+    class Coordinator: NSObject {
+        var value: Binding<Double> //Reference to UISlider value
+        
+        init(value: Binding<Double>) {
+            self.value = value
+        }
+        
+        @objc func valueChanged(_ sender: UISlider) {
+            self.value.wrappedValue = Double(sender.value)
+        }
+    }
+}
+
+struct ColourUISlider_Previews: PreviewProvider {
+    static var previews: some View {
+        ColourUISlider(colour: .red, value: .constant(0.5))
+    }
+}
